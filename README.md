6a8b745c8560d2ba390e8e5e0b723a859b7f2410
cubewhy <cubewhy@163.com>
2024-07-24 08:48:00 +0800

    as template

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 > Not only Lunar
 
 If you don't let me modify it dynamically, I will apply the changes directly to the jar.
+
+This is a template repository.
--- src/main/kotlin/org/cubewhy/patch/Main.kt
+++ src/main/kotlin/org/cubewhy/patch/Main.kt
@@ -4,9 +4,6 @@ import kotlinx.serialization.json.Json
 import org.cubewhy.patch.api.PatchEntry
 import org.cubewhy.patch.config.PatchConfig
 import java.io.File
-import java.io.FileOutputStream
-import java.net.URL
-import java.net.URLClassLoader
 import java.util.jar.JarFile
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
@@ -15,44 +12,40 @@ import kotlin.system.exitProcess
 
 val JSON = Json { ignoreUnknownKeys = true; prettyPrint = true }
 
+class Main
+
+// WARNING: DO NOT modify this file if everything works.
+
 internal fun main(args: Array<String>) {
-    if (args.size != 3) {
-        println("celepatch.jar <in> <out> <script>")
+    if (args.size != 2) {
+        println("celepatch.jar <in> <out>")
         exitProcess(1)
     }
     val inFile = File(args[0])
     val outFile = File(args[1])
-    val scriptFile = File(args[2])
     println("Input: $inFile")
     if (!(inFile.exists() && inFile.isFile)) {
         error("Input file $inFile does not exist")
     }
     val inJar = JarFile(inFile)
-    val scriptJar = JarFile(scriptFile)
-    val scriptUri = URL("file:${scriptFile.absolutePath}")
-
-    val urlClassLoader = URLClassLoader(arrayOf(scriptUri), ClassLoader.getSystemClassLoader())
-    Thread.currentThread().contextClassLoader = urlClassLoader
 
     val fileMap = mutableMapOf<String, ByteArray>()
 
-    with(scriptJar.getInputStream(scriptJar.getEntry("patch.json"))) {
-        val config: PatchConfig = JSON.decodeFromString(this.readAllBytes().decodeToString())
-        val entrypointName = config.entrypoint
-        val entrypoint = Class.forName(entrypointName).getDeclaredConstructor().newInstance() as PatchEntry
+    val config: PatchConfig = JSON.decodeFromString(Main::class.java.getResourceAsStream("/patch.json")!!.readAllBytes().decodeToString())
 
-        inJar.entries().iterator().forEachRemaining { entry ->
-            val out = if (!entry.isDirectory && entry.name.endsWith(".class")) {
-                entrypoint.patchClass(entry.name, inJar.getInputStream(entry).readAllBytes())
-            } else {
-                entrypoint.patchFile(entry.name, inJar.getInputStream(entry).readAllBytes())
-            }
-            // if out equals null, drop the file.
-            if (out != null) {
-                fileMap[entry.name] = out
-            }
+    val entrypoint = Main::class.java.classLoader.loadClass(config.entrypoint).getDeclaredConstructor().newInstance() as PatchEntry
+    inJar.entries().iterator().forEachRemaining { entry ->
+        val out = if (!entry.isDirectory && entry.name.endsWith(".class")) {
+            entrypoint.patchClass(entry.name, inJar.getInputStream(entry).readAllBytes())
+        } else {
+            entrypoint.patchFile(entry.name, inJar.getInputStream(entry).readAllBytes())
+        }
+        // if out equals null, drop the file.
+        if (out != null) {
+            fileMap[entry.name] = out
         }
     }
+
     outFile.createNewFile()
     val zipOut = ZipOutputStream(outFile.outputStream())
     for ((filePath, content) in fileMap.entries) {
